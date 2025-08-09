269c78085ab64777daa6e745c389675925a3dac7
Antonio Junior <antoniojunior.plmt@gmail.com>
2025-08-09 16:39:18 -0400

    first commit - criação das pastas, estrutura inicial, tabelas e super user

---
+++ estoque/admin.py
@@ -0,0 +1,19 @@
+from django.contrib import admin
+from .models import Escola, Item, Movimentacao
+
+@admin.register(Escola)
+class EscolaAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'endereco')
+    search_fields = ('nome',)
+
+@admin.register(Item)
+class ItemAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'quantidade_disponivel', 'quantidade_total')
+    search_fields = ('nome',)
+
+@admin.register(Movimentacao)
+class MovimentacaoAdmin(admin.ModelAdmin):
+    list_display = ('item', 'escola', 'tipo', 'quantidade', 'data')
+    list_filter = ('tipo', 'escola', 'data') # Adiciona filtros na lateral
+    autocomplete_fields = ('item', 'escola') # Melhora a seleção de item e escola
+    
---
+++ estoque/models.py
@@ -0,0 +1,40 @@
+from django.db import models
+from django.utils import timezone # Vamos usar para a data
+
+# Create your models here.
+
+# Tabela ESCOLAS
+class Escola(models.Model):
+    nome = models.CharField(max_length=200, verbose_name="Nome da Escola")
+    endereco = models.TextField(verbose_name="Endereço", null=True, blank=True)
+
+    def __str__(self):
+        return self.nome
+
+# Tabela ITENS
+class Item(models.Model):
+    nome = models.CharField(max_length=200, verbose_name="Nome do Item")
+    descricao = models.TextField(verbose_name="Descrição", null=True, blank=True)
+    quantidade_total = models.PositiveIntegerField(verbose_name="Quantidade Total em Estoque")
+    quantidade_disponivel = models.PositiveIntegerField(verbose_name="Quantidade Disponível")
+    
+    def __str__(self):
+        return f"{self.nome} (Disponível: {self.quantidade_disponivel})"
+
+# Tabela MOVIMENTACAO
+class Movimentacao(models.Model):
+    
+    # Opções para o campo 'tipo'
+    TIPO_CHOICES = [
+        ('SAIDA', 'Saída para Escola'),
+        ('ENTRADA', 'Entrada no Estoque'),
+    ]
+
+    item = models.ForeignKey(Item, on_delete=models.PROTECT, verbose_name="Item")
+    escola = models.ForeignKey(Escola, on_delete=models.PROTECT, verbose_name="Escola")
+    tipo = models.CharField(max_length=10, choices=TIPO_CHOICES, verbose_name="Tipo de Movimentação")
+    quantidade = models.PositiveIntegerField(verbose_name="Quantidade Movimentada")
+    data = models.DateTimeField(default=timezone.now, verbose_name="Data da Movimentação")
+    
+    def __str__(self):
+        return f"{self.get_tipo_display()} de {self.quantidade}x {self.item.nome}"
